00d85679a27522176e8a0c6728537b7eb00b5ba0
sisyou <oddalice4444@gmail.com>
2016-02-20 22:07:10 +0900

    커밋좀 되라

--- src/main/resources/sql.sql
+++ src/main/resources/sql.sql
@@ -12,22 +12,23 @@ agree varchar2(10) not null
 );
 
 --맛집정보 테이블
-drop table rest;
+drop table rest purge;
 create table rest(
 no number(5) NOT NULL,
 nm varchar2(50) PRIMARY KEY,
-addr varchar2(300),
-tel varchar2(20),
-info varchar2(4000),
-site varchar2(80),
-keyword varchar2(100),
-time varchar2(35),
-reserve varchar2(10) NOT NULL,
-delivery varchar2(10) NOT NULL,
-tv varchar2(100),
-link varchar2(100),
-loc varchar2(100),
-eval number(5,1)
+addr varchar2(300), --주소
+tel varchar2(20), --전화번호
+info varchar2(4000), --소개
+site varchar2(80), --음식점 사이트
+keyword varchar2(100), --키워드
+time varchar2(35), --영업시간
+reserve varchar2(10) NOT NULL, --예약
+delivery varchar2(10) NOT NULL, --배달
+tv varchar2(100), -- 방송 프로그램명
+link varchar2(100), --방송링크
+loc varchar2(100), --위도,경도
+indu varchar2(15), --업종
+eval number(5,1) --평점
 );
 
 
